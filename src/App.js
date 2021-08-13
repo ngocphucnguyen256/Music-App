e5e83ddd4d1b00a1778b757252751556ad601b1b
ngocphucnguyen256 <69982260+ngocphucnguyen256@users.noreply.github.com>
2021-08-14 00:58:44 +0700

    deploy

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 
 import Player from './Player';
 import tracks from './tracks';
-
+import {BrowserRouter, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-        <Player tracks={tracks}/>
-    </div>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <div className="App">     
+            <Route exact  path="/">
+                  <h1>Music App</h1>
+                  <Player tracks={tracks}/>
+            </Route>
+       </div>
+    </BrowserRouter>
   );
 }
 
--- src/tracks.js
+++ src/tracks.js
@@ -1,10 +1,13 @@
-import Demons from "./Imagine Dragons - Demons.mp3"
-import CourageToChange from "./Sia - Courage To Change.mp3";
-import CanWeKissForever from "./Kina - Can We Kiss Forever (ft. Adriana Proenza).mp3";
-import DemonsImg from "./Demons.jpg";
-import CourageToChangeImg from "./CourageToChange.jpg";
-import CanWeKissForeverImg from "./CanWeKissForever.jpg";
-
+import Demons from "./assets/Imagine Dragons - Demons.mp3"
+import CourageToChange from "./assets/Sia - Courage To Change.mp3";
+import CanWeKissForever from "./assets/Kina - Can We Kiss Forever (ft. Adriana Proenza).mp3";
+import Birds from "./assets/Imagine Dragons - Birds.mp3"
+import Freak from "./assets/Sub Urban - Freak (feat. REI AMI).mp3"
+import DemonsImg from "./assets/Demons.jpg";
+import CourageToChangeImg from "./assets/CourageToChange.jpg";
+import CanWeKissForeverImg from "./assets/CanWeKissForever.jpg";
+import BirdsImg from "./assets/Birds.jpg";
+import FreakImg from "./assets/Freak.png"
 
 const tracks= [
   {
@@ -28,5 +31,19 @@ const tracks= [
     img: CanWeKissForeverImg,
     color: "#5f9fff",
   },
+  {
+    title :"Birds",
+    artist: "Imagine Dragons",
+    audioSrc: Birds,
+    img: BirdsImg,
+    color: "#c92154"
+  },
+  {
+    title :"Freak",
+    artist: "Sub Urban",
+    audioSrc: Freak,
+    img: FreakImg,
+    color: "#321f41"
+  }
 ];
 export default tracks;
